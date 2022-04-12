d930dadac6229b98e2be33f0be994ef46f9aa842
moonstal1506 <moonstal1506@gmail.com>
2022-04-12 10:55:17 +0900

    단어 뒤집기

--- string/src/string/Ex4.java
+++ string/src/string/Ex4.java
@@ -1,40 +1,26 @@
 package string;
 
-import java.util.ArrayList;
 import java.util.Scanner;
-//Ex4
+//4. 단어 뒤집기
 public class Ex4 {
-	private ArrayList<String> solution(int n,String[] str) {
-		ArrayList<String> answer = new ArrayList<>();
-		for(String x: str) {
-//		String tmp= new StringBuilder(x).reverse().toString();
-//		answer.add(tmp);
-			char[] s =x.toCharArray();
-			int lt=0,rt=x.length()-1;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i =0 ; i<n; i++) {
+//			String a=new StringBuilder(sc.next()).reverse().toString();
+//			System.out.println(a);
+			String a = sc.next();
+			char[] ch=a.toCharArray();
+			int lt=0,rt=ch.length-1;
 			while(lt<rt) {
-				char tmp = s[lt];
-				s[lt]=s[rt];
-				s[rt]=tmp;
+				char tmp =ch[lt];
+				ch[lt]=ch[rt];
+				ch[rt]=tmp;
 				lt++;
 				rt--;
 			}
-			String tmp= String.valueOf(s);
-			answer.add(tmp);
-		}
-		return answer;
-	}
-
-	public static void main(String[] args) {
-		Ex4 T = new Ex4();
-		Scanner kb = new Scanner(System.in);
-		int n = kb.nextInt();
-		String[] str= new String[n];
-		for(int i =0 ; i<n; i++) {
-			str[i]=kb.next();
-		}
-		for(String x: T.solution(n,str)) {
-			System.out.println(x);
+			System.out.println(ch);
 		}
 	}
-
 }
