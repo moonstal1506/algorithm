3cf06125eb1431cc1178e5c97ac612ab11201092
moonstal1506 <moonstal1506@gmail.com>
2022-04-13 13:23:58 +0900

    회문 문자열

--- string/src/string/Ex6.java
+++ string/src/string/Ex6.java
@@ -1,17 +1,18 @@
 package string;
 
 import java.util.Scanner;
+
 //6. 중복문자제거
 public class Ex6 {
-		public static void main(String[] args) {
-			Scanner sc = new Scanner(System.in);
-			String str = sc.next();
-			String answer="";
-			for(int i=0;i<str.length();i++) {
-				if(i==str.indexOf(str.charAt(i))) {
-					answer+=str.charAt(i);
-				}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		String answer = "";
+		for (int i = 0; i < str.length(); i++) {
+			if (i == str.indexOf(str.charAt(i))) {
+				answer += str.charAt(i);
 			}
-			System.out.println(answer);
 		}
+		System.out.println(answer);
 	}
+}
