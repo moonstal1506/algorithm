cb8007a42d42b546553dc15efa38011aaad5a511
moonstal1506 <moonstal1506@gmail.com>
2022-04-14 10:44:16 +0900

    가장 짧은 문자거리

--- string/src/string/Ex10.java
+++ string/src/string/Ex10.java
@@ -1,43 +1,34 @@
 package string;
 
-import java.util.ArrayList;
 import java.util.Scanner;
-
+//10. 가장 짧은 문자거리
 public class Ex10 {
-	private static int[] solution(String str, char t) {
-
-		int[] answer = new int[str.length()];
-		int p = 1000;
-		for (int i = 0; i < str.length(); i++) {
-			if (t == str.charAt(i)) {
-				p = 0;
-				answer[i] = p;
-			} else {
-				p++;
-				answer[i] = p;
-			}
-		}
-
-		p = 1000;
-		for (int i = str.length() - 1; i >= 0; i--) {
-			if (t == str.charAt(i)) {
-				p = 0;
-			} else {
-				p++;
-				answer[i] = Math.min(answer[i], p);
-			}
-		}
-		return answer;
-	}
-
+	
 	public static void main(String[] args) {
-		Scanner kb = new Scanner(System.in);
-		String str = kb.next();
-		char c = kb.next().charAt(0);
-		for (int x : solution(str, c)) {
-			System.out.print(x + " ");
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		char c = sc.next().charAt(0);
+		int p=1000;
+		int[] arr=new int[str.length()];
+		for(int i=0;i<str.length();i++) {
+			if(str.charAt(i)==c) {
+				p=0;
+				arr[i]=p;
+			}else {
+				p++;
+				arr[i]=p;
+			}
 		}
-
+		for(int i=str.length()-1;i>=0;i--) {
+			if(str.charAt(i)==c) {
+				p=0;
+			}else {
+				p++;
+				arr[i]=Integer.min(arr[i], p);
+			}
+		}
+		for(int a: arr)
+		System.out.print(a+" ");
 	}
 
 }
