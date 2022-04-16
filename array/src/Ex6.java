5a4ba91d8e4d4f8693ea737f6bb43eeb521ee62a
moonstal1506 <moonstal1506@gmail.com>
2022-04-16 11:59:51 +0900

    뒤집은 소수

--- array/src/Ex6.java
+++ array/src/Ex6.java
@@ -4,22 +4,36 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 
-
+//6. 뒤집은 소수
 public class Ex6 {
-	
-
-
- 
 	public static void main(String[] args) {
-		Scanner kb = new Scanner(System.in);
-		int n = kb.nextInt();
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
 		int[] arr = new int[n];
 		for(int i=0;i<n;i++) {
-			arr[i]=kb.nextInt();
+			arr[i]=sc.nextInt();
+			int num=0;
+			while(arr[i]>0) {
+				num=num*10+arr[i]%10;
+				arr[i]=arr[i]/10;
+			}
+			if(isPrime(num)) {
+				System.out.print(num+" ");
+			}
 		}
-
-
+		
 		
 	}
 
+	private static boolean isPrime(int num) {
+		if(num==1) {
+			return false;
+		}
+		for(int i=2;i<num;i++) {
+			if(num%i==0) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
