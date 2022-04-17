72ece47f56e1afaf12e629cce492b6c3d5780861
moonstal1506 <moonstal1506@gmail.com>
2022-04-17 12:27:51 +0900

    격자판 최대합

--- array/src/Ex9.java
+++ array/src/Ex9.java
@@ -4,51 +4,39 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 
-//9. °İÀÚÆÇ ÃÖ´ëÇÕ
-//nÁÙÀÇ 2Â÷¿ø¹è¿­¸¸µé¾î ³Ö¾îÁØ´Ù.
-//nÁÙ°ú ¹è¿­³Ñ°Ü
-//answer¿¡ ÃÖ¼Ò°ª
-//sum1,2ÃÊ±âÈ­
-//ÀÌÁß¹İº¹ Çà,¿­
-//´ä¿¡ ÃÖ´ë°ª
-//´Ù½Ã ÃÊ±âÈ­ 
-//´ë°¢¼±
-//´ä¿¡ ÃÖ´ë°ª
+//9. ê²©ìíŒ ìµœëŒ€í•©
 public class Ex9 {
-	public static int solution(int n,int[][] arr) {
-		int answer= Integer.MIN_VALUE;
-		int sum1,sum2;
-		for(int i=0;i<n;i++) {
-			sum1=sum2=0;
-			for(int j=0;j<n;j++) {
-				sum1+=arr[i][j];
-				sum2+=arr[j][i];
-			}
-			answer=Math.max(answer, sum1);
-			answer=Math.max(answer, sum2);
-		}
-		sum1=sum2=0;
-		for(int i=0;i<n;i++) {
-				sum1+=arr[i][i];
-				sum2+=arr[i][n-i-1];
-			
-		}
-		answer=Math.max(answer, sum1);
-		answer=Math.max(answer, sum2);
-		return answer;
-	}
- 
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
 		int n = kb.nextInt();
-		int[][] arr = new int[n][n];
+		int[][] arr= new int[n][n];
 		for(int i=0;i<n;i++) {
 			for(int j=0;j<n;j++) {
 				arr[i][j]=kb.nextInt();
 			}
 		}
-		System.out.print(solution(n,arr));
 		
+		int sum1,sum2;
+		int answer=0;
+		for(int i=0;i<n;i++) {
+			 sum1=sum2=0;
+			for(int j=0;j<n;j++) {
+				sum1+=arr[i][j];
+				sum2+=arr[j][i];
+			}
+			answer=Integer.max(answer, sum1);
+			answer=Integer.max(answer, sum2);
+		}
+		
+		sum1=sum2=0;
+		for(int i=0;i<n;i++) {
+				sum1+=arr[i][i];
+				sum2+=arr[i][n-1-i];
+		}
+		answer=Integer.max(answer, sum1);
+		answer=Integer.max(answer, sum2);
+		
+		System.out.println(answer);
 	}
 
 }
