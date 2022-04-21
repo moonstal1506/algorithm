ca134ca0560008ba6ea0b1a437cb0d982cdb414d
moonstal1506 <moonstal1506@gmail.com>
2022-04-21 14:19:08 +0900

    연속된 자연수의 합2

--- twoPointersSlidingWindow/src/twoPointersSlidingWindow/Ex5.java
+++ twoPointersSlidingWindow/src/twoPointersSlidingWindow/Ex5.java
@@ -8,27 +8,18 @@ public class Ex5 {
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
 		int n = kb.nextInt();
-		int[] arr=new int[n+1];
-		for(int i=1;i<=n;i++) {
-			arr[i]=i;
-		}
-		int sum=0;
-		int cnt=0;
-		int lt=1;
-		int m=n/2+1;
-		for(int rt=1;rt<=m;rt++) {
-			sum+=arr[rt];
-			if(sum==n) {
-				cnt++;
-			}
-			while(sum>=n) {
-				sum-=arr[lt++];
-				if(sum==n) {
-					cnt++;
-				}
+		int answer=0;
+		int cnt=1;
+		n--;
+		while(n>0) {
+			cnt++;
+			n=n-cnt;
+			if(n%cnt==0) {
+				answer++;
 			}
 		}
-		System.out.println(cnt);
+			
+		System.out.println(answer);
 	}
 
 }
