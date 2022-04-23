fbbd727190841507f77d8b2305aa43387673a44d
moonstal1506 <moonstal1506@gmail.com>
2022-04-23 13:50:48 +0900

    모든 아나그램 찾기

--- hashMapTreeSet/src/hashMapTreeSet/Ex4.java
+++ hashMapTreeSet/src/hashMapTreeSet/Ex4.java
@@ -1,49 +1,36 @@
 package hashMapTreeSet;
 
-import java.util.Scanner;
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 
-//4.¸ðµç ¾Æ³ª±×·¥Ã£±â
-//¹®ÀÚ¿­ µÎ°³ ÀÔ·Â¹Þ±â
-//´ä ÃÊ±âÈ­
-//ÇØ½¬¸Ê µÎ°³¸¸µé¾î
-//b¹®ÀÚ¿­ÀÇ Å°¿Í°ª
-//a¸ÊÀº b±æÀÌ-1¸¸Å­ ¼¼ÆÃÇØ³õ°í rt¿òÁ÷¿©
-//°°ÀºÁö ºñ±³ ´ä++
-//lt»©ÁÖ±â
-//0ÀÌ¸é Å°¿¡¼­ »èÁ¦
-//lt++
+//4. ëª¨ë“  ì•„ë‚˜ê·¸ëž¨ ì°¾ê¸°
 public class Ex4 {
-	private static int solution(String a, String b) {
-		int answer = 0;
-		HashMap<Character, Integer> am = new HashMap<>();
-		HashMap<Character, Integer> bm = new HashMap<>();
-		for (char x : b.toCharArray())
-			bm.put(x, bm.getOrDefault(x, 0) + 1);
-		int L = b.length() - 1;
-		for (int i = 0; i < L; i++)
-			am.put(a.charAt(i), am.getOrDefault(a.charAt(i), 0) + 1);
-		int lt = 0;
-		for (int rt = L; rt < a.length(); rt++) {
-			am.put(a.charAt(rt), am.getOrDefault(a.charAt(rt), 0) + 1);
-			if (am.equals(bm))
-				answer++;
-			am.put(a.charAt(lt), am.get(a.charAt(lt)) - 1);
-			if (am.get(a.charAt(lt)) == 0)
-				am.remove(a.charAt(lt));
-			lt++;
-		}
-		return answer;
-	}
 
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
 		String a = kb.next();
 		String b = kb.next();
-
-		System.out.print(solution(a, b));
-
+		HashMap<Character, Integer> map =new HashMap<>();
+		HashMap<Character, Integer> map2 =new HashMap<>();
+		int answer=0;
+		int lt=0;
+		for(char key: b.toCharArray()) {
+			map2.put(key, map2.getOrDefault(key, 0)+1);
+		}
+		for(int i =0;i<b.length()-1;i++) {
+			map.put(a.charAt(i), map.getOrDefault(a.charAt(i), 0)+1);
+		}
+		for(int rt=b.length()-1;rt<a.length();rt++) {
+			map.put(a.charAt(rt), map.getOrDefault(a.charAt(rt), 0)+1);
+			if(map.equals(map2)) {
+				answer++;
+			}
+			map.put(a.charAt(lt), map.get(a.charAt(lt))-1);
+			if(map.get(a.charAt(lt))==0) {
+				map.remove(a.charAt(lt));
+			}
+			lt++;
+		}
+		System.out.println(answer);
 	}
-
 }
