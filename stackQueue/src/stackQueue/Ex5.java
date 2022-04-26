212a7ee477002b0b5449e97eeaa46b0a74caf22a
moonstal1506 <moonstal1506@gmail.com>
2022-04-26 12:57:38 +0900

    쇠막대기

--- stackQueue/src/stackQueue/Ex5.java
+++ stackQueue/src/stackQueue/Ex5.java
@@ -3,36 +3,27 @@ package stackQueue;
 import java.util.Scanner;
 import java.util.Stack;
 
-//5. ¼è¸·´ë±â
-//¹®ÀÚ¿­ÀÔ·Â¹Ş¾Æ ¼ıÀÚ¸®ÅÏ
-//´ä 0ÃÊ±âÈ­
-//½ºÅÃ¸¸µé¾î
-//¹®ÀÚ±æÀÌ¸¸Å­ µ¹·Á
-//(¸é ½ºÅÃ¿¡ ´ã¾Æ
-//)¸é »©°í 
-//¹Ù·Î¾Õ (¸é ½ºÅÃÅ©±â¸¸Å­ ´õÇØ
-//¾Æ´Ï¸é ²Ç´Ù¸®ÇÏ³ª ++
+//5. ì‡ ë§‰ëŒ€ê¸°
 public class Ex5 {
-	private static int solution(String str) {
-		int answer = 0;
-		Stack<Character> stack=new Stack<>();
-		for(int i=0;i<str.length();i++) {
-			if(str.charAt(i)=='(') stack.push('(');
-			else {
-				stack.pop();
-				if(str.charAt(i-1)=='(') answer+=stack.size();
-				else answer++;
-			}
-			
-		}
-		return answer;
-	}
- 
+
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
 		String str = kb.next();
-		System.out.print(solution(str));
-		
+		Stack<Character> stack= new Stack<>();
+		int answer=0;
+		for(int i=0;i<str.length();i++) {
+			if(str.charAt(i)=='(') {
+				stack.push('(');
+			}else {
+				stack.pop();
+				if(str.charAt(i-1)=='(') {
+					answer+=stack.size();
+				}else {
+					answer++;
+				}
+			}
+		}
+		System.out.println(answer);
 	}
 
 }
