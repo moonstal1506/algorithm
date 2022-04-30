2483f79740551506cccdc61b0e6ba813dd252f2f
moonstal1506 <moonstal1506@gmail.com>
2022-04-30 11:52:34 +0900

    좌표정렬

--- sortingSearching/src/sortingSearching/Ex7.java
+++ sortingSearching/src/sortingSearching/Ex7.java
@@ -4,35 +4,39 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
-//7. ÁÂÇ¥Á¤·Ä
+//7. ì¢Œí‘œ ì •ë ¬
 
 class Point implements Comparable<Point>{
-	
-	public int x, y;
-	Point(int x, int y){
+	int x,y;
+
+	public Point(int x, int y) {
 		this.x=x;
 		this.y=y;
 	}
+
 	@Override
 	public int compareTo(Point o) {
-		if(this.x==o.x) return this.y-o.y;
-		else return this.x-o.x;
+		if(o.x==x) {
+			return y-o.y;
+		}else {
+			return x-o.x;
+		}
 	}
-	
 }
+
 public class Ex7 {
-	
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
 		int n = kb.nextInt();
-		ArrayList<Point> arr = new ArrayList<>();
+		ArrayList<Point> arr= new ArrayList<>();
 		for(int i=0;i<n;i++) {
 			int x=kb.nextInt();
 			int y=kb.nextInt();
 			arr.add(new Point(x,y));
 		}
 		Collections.sort(arr);
-		for(Point o:arr) System.out.println(o.x+" "+o.y);
+		for(Point o: arr) {
+			System.out.println(o.x+" "+o.y);
+		}
 	}
-
 }
