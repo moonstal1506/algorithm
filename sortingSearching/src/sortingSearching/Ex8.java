4444b0415f6be385f705d74c23b7d2d7a1b16ba1
moonstal1506 <moonstal1506@gmail.com>
2022-04-30 17:48:21 +0900

    이분검색

--- sortingSearching/src/sortingSearching/Ex8.java
+++ sortingSearching/src/sortingSearching/Ex8.java
@@ -3,39 +3,34 @@ package sortingSearching;
 import java.util.Arrays;
 import java.util.Scanner;
 
-//8. ÀÌºĞ°Ë»ö
-//n°³ ¼ıÀÚ mÀ» Ã£¾Æ¼­ À§Ä¡¸®ÅÏ
-//¿À¸§Â÷¼ø Á¤·ÄÈÄ lt rt
-//rt°¡ Å©°Å³ª °°À» ¶§ ±îÁö
-//Àı¹İÀ» ÂÉ°³ m°ú °°ÀºÁö ºñ±³ ÇØ¼­ ¸ÂÀ¸¸é ´ä
-//mº¸´Ù Å©¸é rt¸¦ ÁÙÀÌ°í
-//ÀÛÀ¸¸é lt¸¦ ´Ã¸°´Ù.
+//8. ì´ë¶„ê²€ìƒ‰
 
 public class Ex8 {
-	
-	private static int solution(int n,int m, int[] arr) {
-		int answer=0;
-		Arrays.sort(arr);
-		int lt=0, rt=n-1;
-		while(lt<=rt) {
-			int mid=(lt+rt)/2;
-			if(arr[mid]==m) {
-				answer=mid+1;
-				break;
-			}
-			if(arr[mid]>m) rt=mid-1;
-			else lt=mid+1;
-		}
-		return answer;
-	}
-	
+
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
 		int n = kb.nextInt();
 		int m = kb.nextInt();
-		int[] arr=new int[n];
-		for(int i=0;i<n;i++) arr[i]=kb.nextInt();
-		System.out.println(solution(n, m, arr));
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = kb.nextInt();
+		}
+		Arrays.sort(arr);
+		int lt = 0;
+		int rt = n - 1;
+		int answer = 0;
+		while (lt <= rt) {
+			int mid = (lt + rt) / 2;
+			if (arr[mid] == m) {
+				answer = mid + 1;
+				break;
+			} else if (arr[mid] > m) {
+				rt = mid - 1;
+			} else {
+				lt= mid+1;
+			}
+		}
+		System.out.println(answer);
 	}
 
 }
