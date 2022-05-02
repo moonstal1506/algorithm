bcc1faecf27ba55349d157729c8fc3ef2ef8dd7b
moonstal1506 <moonstal1506@gmail.com>
2022-05-02 19:25:16 +0900

    팩토리얼

--- dfs_bfs/src/Ex3.java
+++ dfs_bfs/src/Ex3.java
@@ -4,17 +4,16 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-//3. ÆÑÅä¸®¾ó
-//n°íÁ¤½ÃÄÑ³õ°í Àç±Í·Î µ¹·Á
+//3. íŒ©í† ë¦¬ì–¼
 public class Ex3 {
-	public int DFS(int n){
-		if(n==1) return 1;
-		else if(n==2) return 1;
-		else return DFS(n-2)+DFS(n-1);
+	public static int DFS(int n){
+		if(n==1) {
+			return 1;
+		}else {
+			return n*DFS(n-1);
+		}
 	}
 	public static void main(String[] args){
-		Ex3 T = new Ex3();
-		int n=10;
-		for(int i=1; i<=n; i++) System.out.print(T.DFS(i)+" ");
+		System.out.println(DFS(5));
 	}	
 }
