cc3eb5e20cc2e270467631543c95445181dddcfa
moonstal1506 <moonstal1506@gmail.com>
2022-05-05 10:35:28 +0900

    송아지 찾기

--- dfs_bfs/src/Ex8.java
+++ dfs_bfs/src/Ex8.java
@@ -2,46 +2,43 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-//8.¼Û¾ÆÁö Ã£±â
-//Áßº¹µÈ°Ç ´Ù½Ã ³ÖÁö ¾Ê´Â´Ù.
+//8. ì†¡ì•„ì§€ ì°¾ê¸°1(BFS)
 public class Ex8 {
-	int answer=0;
-	int[] dis={1, -1, 5};
-	int[] ch;//Ã¼Å©¹è¿­
-	Queue<Integer> Q = new LinkedList<>();
-	public int BFS(int s, int e){
-		ch=new int[10001];//ÁÂÇ¥°¡ 1ºÎÅÍ 10000±îÁö
-		ch[s]=1;//Ã¹¹øÂ° °ª µé¾î¿Â °Í Ã¼Å©
-		Q.offer(s);
-		int L=0;
-		while(!Q.isEmpty()){
-			int len=Q.size();
-			for(int i=0; i<len; i++){
-				int x = Q.poll();
-//				if(nx==e){
-//					return L;
-//				} °¡´ÉÇÏÁö¸¸ ºÎ¸ð·¹º§¿¡¼­ ÇÏ´Â°Ô ´õ ÁÁ´Ù
-				for(int j=0; j<3; j++){//¿òÁ÷ÀÌ±â=ÀÚ½Ä±¸ÇÏ±â
-					int nx=x+dis[j];
-					if(nx==e){
-						return L+1;//ÀÚ½Ä¿¡¼­ ¹Ù·Î ¸®ÅÏÇØ¾ß ´õ ÁÁ´Ù ÇöÀç ºÎ¸ð·¹º§ÀÌ¹Ç·Î+1
+
+	int[] ch = new int[10001];
+	Queue<Integer> q = new LinkedList<>();
+	int[] dis = { 1, -1, 5 };
+
+	public int BFS(int s, int e) {
+		q.add(s);
+		ch[s]=1;
+		int L = 0;
+		while(!q.isEmpty()) {
+			int len=q.size();
+			for(int i=0; i<len;i++) {
+				int x=q.poll();
+				for(int d: dis) {
+					int nx=x+d;
+					if(nx==e) {
+						return L+1;
 					}
-					if(nx>=1 && nx<=10000 && ch[nx]==0){ 
-						ch[nx]=1; //Áßº¹µÇÁö ¾ÊÀº ¼ö¿¡ 1·Î Ã¼Å©
-						Q.offer(nx);//Å¥¿¡ ³Ö¾îÁÜ
+					if(1<=nx && nx<=10000 && ch[nx]==0) {
+						ch[nx]=1;
+						q.add(nx);
 					}
 				}
 			}
 			L++;
 		}
-		return 0;
+		return L;
+
 	}
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		Ex8 T = new Ex8();
 		Scanner kb = new Scanner(System.in);
-		int s=kb.nextInt();
-		int e=kb.nextInt();
+		int s = kb.nextInt();
+		int e = kb.nextInt();
 		System.out.println(T.BFS(s, e));
-	}	
+	}
 }
