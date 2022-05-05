cb8e3900431735dbde168379115790c8b2812e41
moonstal1506 <moonstal1506@gmail.com>
2022-05-05 19:20:11 +0900

    짧은 경로(BFS)

--- dfs_bfs/src/Ex10.java
+++ dfs_bfs/src/Ex10.java
@@ -2,33 +2,41 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-//10.¸»´Ü³ëµå±îÁöÀÇ °¡Àå ÂªÀº °æ·Î BFS
+//10. Tree ë§ë‹¨ë…¸ë“œê¹Œì§€ì˜ ê¹Œì¥ ì§§ì€ ê²½ë¡œ(BFS)
 public class Ex10 {
-	 Node root; 
-		public int BFS(Node root){ 
-			Queue<Node> Q=new LinkedList<>();
-			Q.offer(root);
-			int L=0;
-			while(!Q.isEmpty()){
-				int len=Q.size();
-				for(int i=0; i<len; i++){//Å¥¾È¿¡ °³¼ö¸¸Å­ µ¹·Á
-					Node cur=Q.poll();//ÀÚ½Ä³ëµå ÀÖ´ÂÁö È®ÀÎ
-					if(cur.lt==null && cur.rt==null) return L;//¸»´Ü ³ëµå¸é ¸®ÅÏ
-					if(cur.lt!=null) Q.offer(cur.lt);//ÀÖÀ¸¸é µÚ¿¡ ³Ö¾îÁÜ
-					if(cur.rt!=null) Q.offer(cur.rt);
+	Node root;
+
+	public int BFS(Node root) {
+		Queue<Node> q = new LinkedList<>();
+		q.add(root);
+		int L = 0;
+		while (!q.isEmpty()) {
+			int len = q.size();
+			for (int i = 0; i < len; i++) {
+				Node node = q.poll();
+				if(node.lt==null && node.rt==null) {
+					return L;
 				}
-				L++;
+				if(node.lt != null) {
+					q.add(node.lt);
+				}
+				if(node.rt != null) {
+					q.add(node.rt);
+				}
 			}
-			return 0;//¾Ï°Å³ª
-	    } 
-	  
-	    public static void main(String args[]) { 
-	    	Ex10 tree=new Ex10(); 
-	        tree.root=new Node(1); 
-	        tree.root.lt=new Node(2); 
-	        tree.root.rt=new Node(3); 
-	        tree.root.lt.lt=new Node(4); 
-	        tree.root.lt.rt=new Node(5); 
-	        System.out.println(tree.BFS(tree.root)); 
-	    } 
-	} 
+			L++;
+		}
+
+		return L;
+	}
+
+	public static void main(String args[]) {
+		Ex10 tree = new Ex10();
+		tree.root = new Node(1);
+		tree.root.lt = new Node(2);
+		tree.root.rt = new Node(3);
+		tree.root.lt.lt = new Node(4);
+		tree.root.lt.rt = new Node(5);
+		System.out.println(tree.BFS(tree.root));
+	}
+}
