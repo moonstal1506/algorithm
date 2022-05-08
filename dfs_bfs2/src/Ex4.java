0bafbc3f73ab9c97963ef67f6d2f0d7e0eb82baf
moonstal1506 <moonstal1506@gmail.com>
2022-05-08 13:36:33 +0900

    중복순열

--- dfs_bfs2/src/Ex4.java
+++ dfs_bfs2/src/Ex4.java
@@ -1,28 +1,31 @@
 
 import java.util.Scanner;
 
-//4. Áßº¹¼ø¿­
+//4.ì¤‘ë³µìˆœì—´
 public class Ex4 {
 	static int[] pm;
 	static int n, m;
-	public void DFS(int L){
-		if(L==m){//´Ù»Ì¾ÒÀ» ¶§ 2°¡ µÇ¸é
-			for(int x : pm) System.out.print(x+" ");
+
+	public void DFS(int L) {// 0->1->2
+		if (L == m) {// 2
+			for (int x : pm) {
+				System.out.print(x + " ");
+			}
 			System.out.println();
-		}
-		else{
-			for(int i=1; i<=n; i++){//Áßº¹ÀÌ´Ï±î n¹ø
-				pm[L]=i;//L¿¡´Â 0,1±îÁö 2°¡µÇ¸é ¸®ÅÏµÇ´Ï±î ¿©±â±îÁö ¾È¿Â´Ù i´Â 123
-				DFS(L+1);
+		} else {
+			for (int i = 1; i <= n; i++) {
+				pm[L] = i;// 0 1->1 1
+				DFS(L + 1);// 1->2
 			}
 		}
 	}
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		Ex4 T = new Ex4();
 		Scanner kb = new Scanner(System.in);
-		n=kb.nextInt();
-		m=kb.nextInt();
-		pm=new int[m];
+		n = kb.nextInt();
+		m = kb.nextInt();
+		pm = new int[m];
 		T.DFS(0);
 	}
 }
