a1fbd6b2f5119a7226a8c6b16595115edd15207d
moonstal1506 <moonstal1506@gmail.com>
2022-05-10 11:35:26 +0900

    조합 구하기

--- dfs_bfs2/src/Ex9.java
+++ dfs_bfs2/src/Ex9.java
@@ -3,20 +3,20 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
 
-//9.조합 구하기
-//순서가 상관이 없다
+//9. 議고빀 援ы븯湲�
 public class Ex9 {
 	static int[] combi;
-	static int n, m; //n개중 m개를 뽑아라
+	static int n, m;
 	public void DFS(int L, int s){
-		if(L==m){//2개다 만들어지면
-			for(int x : combi) System.out.print(x+" ");
+		if(L==m) {
+			for(int x:combi) {
+				System.out.print(x+" ");
+			}
 			System.out.println();
-		}
-		else{
-			for(int i=s; i<=n; i++){//1부터 n가닥
+		}else {
+			for(int i=s;i<=n;i++) {
 				combi[L]=i;
-				DFS(L+1, i+1);//i+1은 반복문i로 들어가는데 조합은 또 뽑을 필요 없으니까 앞에서 한건 안해도 돼
+				DFS(L+1,i+1);
 			}
 		}
 	}
@@ -26,6 +26,6 @@ public class Ex9 {
 		n=kb.nextInt();
 		m=kb.nextInt();
 		combi=new int[m];
-		T.DFS(0, 1);//0개 숫자 1뽑을 것
+		T.DFS(0, 1);
 	}
 }
