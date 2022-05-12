d740634044751beeb69d056a3ac455e0bc1f112e
moonstal1506 <moonstal1506@gmail.com>
2022-05-12 10:53:36 +0900

    토마토

--- dfs_bfs2/src/dfs_bfs2/Ex12.java
+++ dfs_bfs2/src/dfs_bfs2/Ex12.java
@@ -1,69 +1,76 @@
 package dfs_bfs2;
 
-
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-//12.�丶��
+//12. 토마토(BFS)
 public class Ex12 {
-	static int[] dx={-1, 0, 1, 0};
-	static int[] dy={0, 1, 0, -1};
+	static int[] dx = { -1, 0, 1, 0 };
+	static int[] dy = { 0, 1, 0, -1 };
 	static int[][] board, dis;
 	static int n, m;
-	static Queue<Point> Q=new LinkedList<>();//main������ ���� static
-	public void BFS(){
-		while(!Q.isEmpty()){
-			Point tmp=Q.poll();
-			for(int i=0; i<4; i++){
-				int nx=tmp.x+dx[i];
-				int ny=tmp.y+dy[i];
-				if(nx>=0 && nx<n && ny>=0 && ny<m && board[nx][ny]==0){
-					board[nx][ny]=1;
-					Q.offer(new Point(nx, ny));//������ �־��ְ�
-					dis[nx][ny]=dis[tmp.x][tmp.y]+1;// ��¥+1
+	static Queue<Point> q = new LinkedList<>();
+
+	public void BFS() {
+		while (!q.isEmpty()) {
+			Point p = q.poll();
+			for (int i = 0; i < 4; i++) {
+				int nx = p.x + dx[i];
+				int ny = p.y + dy[i];
+				if (0 <= nx && nx < n && 0 <= ny && ny < m && board[nx][ny] == 0) {
+					board[nx][ny] = 1;
+					q.add(new Point(nx,ny));
+					dis[nx][ny]=dis[p.x][p.y]+1;
 				}
 			}
-		}	
+
+		}
 	}
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		Ex12 T = new Ex12();
 		Scanner kb = new Scanner(System.in);
-		m=kb.nextInt();
-		n=kb.nextInt();
-		board=new int[n][m];
-		dis=new int[n][m];
-		for(int i=0; i<n; i++){
-			for(int j=0; j<m; j++){
-				board[i][j]=kb.nextInt();
-				if(board[i][j]==1) Q.offer(new Point(i, j));//���� �͵� ��ġ �־�α�
+		m = kb.nextInt();
+		n = kb.nextInt();
+		board = new int[n][m];
+		dis = new int[n][m];
+		// board채우면서 1이면 큐넣기
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				board[i][j] = kb.nextInt();
+				if (board[i][j] == 1) {
+					q.add(new Point(i, j));
+				}
 			}
 		}
+
+		// BFS호출
 		T.BFS();
-		boolean flag=true;
-		int answer=Integer.MIN_VALUE;
-		for(int i=0; i<n; i++){
-			for(int j=0; j<m; j++){
-				if(board[i][j]==0) flag=false;//���� ������ �ִ�
+		// board 0찾기 안익은거 있으면 false
+		boolean flag = true;
+		for(int i=0; i<n;i++) {
+			for(int j=0;j<m;j++) {
+				if(board[i][j]==0) {
+					flag=false;
+				}
 			}
 		}
-		if(flag){//�;�����
-			for(int i=0; i<n; i++){
-				for(int j=0; j<m; j++){
-					answer=Math.max(answer, dis[i][j]);//dis�ǿ� ���� ��¥ ������
+		
+		// 다익었으면 거리 최소값
+		int answer=0;
+		if(flag) {
+			for(int i=0; i<n;i++) {
+				for(int j=0;j<m;j++) {
+					answer=Math.max(answer, dis[i][j]);
 				}
 			}
 			System.out.println(answer);
+		}else {
+			System.out.println(-1);
 		}
-		else System.out.println(-1);
+
+		// 아니면 -1
+
 	}
 }
-
-//class Point{
-//	public int x, y;
-//	Point(int x, int y){
-//		this.x=x;
-//		this.y=y;
-//	}
-//}
