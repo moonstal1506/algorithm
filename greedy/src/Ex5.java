cf1f891630df01f4131fa43b4310ecd5b7e38da0
moonstal1506 <moonstal1506@gmail.com>
2022-05-16 14:06:31 +0900

    다익스트라 알고리즘

--- greedy/src/Ex5.java
+++ greedy/src/Ex5.java
@@ -4,54 +4,8 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
-//5.´ÙÀÍ½ºÆ®¶ó ¾Ë°í¸®Áò
-public class Ex5 {
-	static int n, m;
-	static ArrayList<ArrayList<Edge>> graph;
-	static int[] dis;
-	public void solution(int v){
-		PriorityQueue<Edge> pQ = new PriorityQueue<>();
-		pQ.offer(new Edge(v, 0));//1¹ø Á¤Á¡ °¡´Âµ¥ °¡ÁßÄ¡ 0
-		dis[v]=0;//±×·¡¼­ 0
-		while(!pQ.isEmpty()){
-			Edge tmp=pQ.poll();//°¡ÁßÄ¡ ÀÛÀº°Í ºÎÅÍ ³ª¿Ã°ÅÀÓ
-			int now=tmp.vex;//1
-			int nowCost=tmp.cost;//0
-			if(nowCost>dis[now]) continue;//0>0 /(2,11)(2,12)°¡ ÀÖÀ» ¶§ ÀÌ¹Ì 11Çß´Âµ¥ 12ÇÒ ÇÊ¿ä ¾ø´Ù.
-			for(Edge ob : graph.get(now)){//1ÀÌ °¥¼öÀÖ´Â Á¤Á¡°ú °¡ÁßÄ¡(2,12)(3,4)
-				if(dis[ob.vex]>nowCost+ob.cost){//dis[2]´Â ¹«ÇÑ>0+12 Ã³À½Àº ¹«ÇÑÀÌ´Ï±î ¹«Á¶°Ç ´ã±è
-					dis[ob.vex]=nowCost+ob.cost;//µ¤¾î¾²±â
-					pQ.offer(new Edge(ob.vex, nowCost+ob.cost));//±×¸®°í pq¿¡ Á¤Á¡°ú ÃÖ¼Òºñ¿ëÀÌ¶ó°í »ý°¢ÇÑ °ª ³Ö¾îÁÜ
-				}
-			}
-		}
-	}
-
-	public static void main(String[] args){
-		Ex5 T = new Ex5();
-		Scanner kb = new Scanner(System.in);
-		n=kb.nextInt();//Á¤Á¡¼ö
-		m=kb.nextInt();//°£¼±¼ö
-		graph = new ArrayList<ArrayList<Edge>>();
-		for(int i=0; i<=n; i++){
-			graph.add(new ArrayList<Edge>());
-		}
-		dis=new int[n+1];//Á¤Á¡ 1ºÎÅÍ½ÃÀÛ
-		Arrays.fill(dis, Integer.MAX_VALUE);//°¡Àå Å«°ªÀ¸·Î ÃÊ±âÈ­
-		for(int i=0; i<m; i++){
-			int a=kb.nextInt();
-			int b=kb.nextInt();
-			int c=kb.nextInt();
-			graph.get(a).add(new Edge(b, c));//a°¡ °¥¼ö ÀÖ´Â ¹æÇâ°ú °¡ÁßÄ¡
-		}
-		T.solution(1);//1¹ø Á¤Á¡
-		for(int i=2; i<=n; i++){
-			if(dis[i]!=Integer.MAX_VALUE) System.out.println(i+" : "+dis[i]);
-			else System.out.println(i+" : impossible");
-		}
-	}
-}
-
+//5. ë‹¤ìµìŠ¤íŠ¸ë¼ ì•Œê³ ë¦¬ì¦˜
+//ë¹„ìš© ì˜¤ë¦„ì°¨ìˆœ
 class Edge implements Comparable<Edge>{
     public int vex;
 	public int cost;
@@ -64,3 +18,54 @@ class Edge implements Comparable<Edge>{
         return this.cost-ob.cost;
     }
 }
+
+public class Ex5 {
+	static int n, m;
+	static ArrayList<ArrayList<Edge>> graph;
+	static int[] dis;
+
+	public static void main(String[] args){
+		Scanner kb = new Scanner(System.in);
+		n=kb.nextInt();
+		m=kb.nextInt();
+		graph = new ArrayList<ArrayList<Edge>>();
+		for(int i=0; i<=n; i++){
+			graph.add(new ArrayList<Edge>());
+		}
+		dis=new int[n+1];
+		Arrays.fill(dis, Integer.MAX_VALUE);
+		for(int i=0; i<m; i++){
+			int a=kb.nextInt();
+			int b=kb.nextInt();
+			int c=kb.nextInt();
+			graph.get(a).add(new Edge(b, c));
+		}
+		
+		//qì— ì—£ì§€ ë„£ìŒ, dis[1]=0 që½‘ëŠ”ê±° ë°˜ë³µ ì •ì  ë¹„ìš© 
+		//ë½‘ì€ í˜„ìž¬ ë¹„ìš©ì´ ì§€ê¸ˆ disë¹„ìš©ë³´ë‹¤ í¬ë©´ íŒ¨ìŠ¤
+		//ì •ì ì— ë‹¬ë¦° ì—£ì§€ ëŒë ¤ *ë‹¤ìŒvexì˜ dis! í˜„ìž¬ë¹„ìš©+ë‹¤ìŒ ë¹„ìš© ë” ìž‘ìœ¼ë©´ ë‹¤ìŒ ì—£ì§€ ì¶”ê°€
+		PriorityQueue<Edge> q = new PriorityQueue<>();
+		q.add(new Edge(1,0));
+		dis[1]=0;
+		while(!q.isEmpty()) {
+			Edge edge = q.poll();
+			int nowCost=edge.cost;
+			int nowVex=edge.vex;
+			if(nowCost>dis[nowVex]) {
+				continue;
+			}
+			for(Edge e: graph.get(nowVex)) {
+				if(dis[e.vex]>nowCost+e.cost) {
+					dis[e.vex]=nowCost+e.cost;
+					q.add(new Edge(e.vex,nowCost+e.cost));
+				}
+			}
+		}
+		
+		for(int i=2; i<=n; i++){
+			if(dis[i]!=Integer.MAX_VALUE) System.out.println(i+" : "+dis[i]);
+			else System.out.println(i+" : impossible");
+		}
+	}
+}
+
